d7243c3b960163837269388f90405fefab860a73
Anirban sengupta <sengupta.ani.80@gmail.com>
2019-08-21 11:40:41 +0530

    DAO

--- JDBCSpring/src/main/java/springjdbc/demo/JDBCSpring/EmployeeDao.java
+++ JDBCSpring/src/main/java/springjdbc/demo/JDBCSpring/EmployeeDao.java
@@ -17,9 +17,9 @@ public int updateEmployee(Employee e){
     String query="update employee set   
     name='"+e.getName()+"',salary='"+e.getemail()+"' where id='"+e.getId()+"' ";  
     return jdbcTemplate.update(query);  
-}  
+ }  
 public int deleteEmployee(Employee e){  
     String query="delete from employee where id='"+e.getId()+"' ";  
     return jdbcTemplate.update(query);  
-}  
+ }  
 }
